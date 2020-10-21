45a7368738d566029249e1f564cb282c6f2faab0
John Morrow <cras@craswork>
2020-10-20 17:47:46 -0700

    added to data conditioning (old commit)

--- asterisk_3_dataConditioning.py
+++ asterisk_3_dataConditioning.py
@@ -10,6 +10,7 @@ This file will...
 
 import csv
 import pandas as pd
+import numpy as n
 
 def convert_to_mm(val):
     return val * 1000 #TODO: make it only return a certain number of sig figs
@@ -18,16 +19,18 @@ def normalize_vertical_data(val, depth):
     return val / depth
 
 def normalize_horizontal_data(val, span):
-    return val / depth
+    return val / span
 
-def normalize_diagonal_data(val, span, depth):
-    pass
+def normalize_diagonal_data(xval, yval, span, depth):
+    x_norm = normalize_horizontal_data(xval, span)
+    y_norm = normalize_vertical_data(yval, depth)
+    return x_norm, y_norm
 
-def filter_data(data):
-    #moving average
-    filtered_data = data
-
-    return filtered_data
+def filter_data(data, window_length = 3):
+    #moving average as seen here: https://www.kite.com/python/answers/how-to-find-moving-average-from-data-points-in-python
+    cumulative_sum = np.cumsum(np.insert(data, 0, 0))
+    moving_averages = (cumulative_sum[window_length:] - cumulative_sum[:-window_length]) / window_length
+    return moving_averages #test this out - double check it
 
 
 if __name__ == "__main__":
