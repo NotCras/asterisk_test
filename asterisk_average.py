I think I fixed average deviation to the point where its not horrible. Looks ok, so moving on for now

asterisk_average.py
@@ -329,18 +329,20 @@ class AveragedTrial(AsteriskTrialData):
         plt.title(f"Debugging the calculated points at {i}")
         plt.show()
 
-    def calculate_avg_dev(self, all_points=None, sample_points=25, show_debug=False, show_pt_debug=True):
+    def calculate_avg_dev(self, all_points=None, all_avgs=None, sample_points=25, show_debug=False, show_pt_debug=False):
         """
         Goes through our set of averages and calculates the average deviation of the trials for each avg point
         """
         # TODO: check that we actually have averages
-        avg_pts = self.poses
         avg_ads = pd.DataFrame()
         avg_ads_up = pd.DataFrame()
         avg_ads_down = pd.DataFrame()
 
         if all_points is None:
             all_points = self.all_points  # for now, setting this just in case
+
+        if all_avgs is None:
+            avg_pts = self._rotate_points(self.poses, self.rotations[self.trial_translation])
 
         r_target_x, r_target_y = AsteriskPlotting.get_c(sample_points)
         rotated_target_line = np.column_stack((r_target_x, r_target_y))

asterisk_hand_data.py
@@ -224,8 +224,10 @@ class AsteriskHandData:
 
         # plot target lines as dotted lines
         self.plot_all_target_lines(colors)
-        plt.xticks(np.linspace(-0.6, 0.6, 11), rotation=30)
-        plt.yticks(np.linspace(-0.6, 0.6, 11))
+        plt.title(f"{self.hand.get_name()} avg asterisk, rot: {trials[0].trial_rotation}")
+        plt.xticks(np.linspace(-0.6, 0.6, 13), rotation=30)
+        plt.yticks(np.linspace(-0.6, 0.6, 13))
+        plt.gca().set_aspect('equal', adjustable='box')
         return plt
 
     def _make_fd_plot(self, trials):