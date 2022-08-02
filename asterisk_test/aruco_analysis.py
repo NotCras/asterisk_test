df687d2963b45a0c00bb83475075380a3c9d2f70
john <morrowjo@oregonstate.edu>
2022-08-01 18:04:32 -0700

    switching aruco analysis implementation to use my arcuo_tool package

---
+++ asterisk_test/aruco_analysis.py
@@ -0,0 +1,29 @@
+"""
+Handles the aruco analysis for a single trial using my custom aruco_tool package.
+"""
+
+from aruco_tool import ArucoFunc
+
+class AstArucoAnalysis():
+
+    def __init__(self, file_loc_obj, camera_calib, camera_dists, marker_side_dims):
+        pass
+        self.af = ArucoFunc(camera_calib, camera_dists, marker_side_dims)
+
+        self.aruco_pics_loc = file_loc_obj.aruco_pics
+        self.aruco_data_loc = file_loc_obj.aruco_data
+
+    def aruco_analyze_trial(self, trial_name, id):
+        """
+        Analyzes a folder of aruco images
+        """
+        trial_folder = self.aruco_pics_loc + trial_name #todo:double check that this is correct
+        aruco_loc = self.af.full_Analysis_single_id(trial_folder, id)
+
+        result_folder = self.aruco_data_loc + trial_name
+        aruco_loc.save_poses(file_name_overwrite=result_folder)
+
+
+
+
+
---
+++ asterisk_test/file_manager.py
@@ -0,0 +1,35 @@
+"""
+Class which stores the locations of files.
+"""
+
+
+from pathlib import Path
+
+class AstDirectory():
+
+    def __init__(self):
+        self.compressed_data = None
+        self.aruco_pics = None
+        self.aruco_data = None
+        self.path_data = None
+        self.metric_results = None
+        self.result_figs = None
+        self.debug_figs = None
+
+    def file_location(self):
+        """
+        Function enables user to edit a file location. Function handles pathlib stuff
+        """
+        pass
+
+    def import_locations(self, file_loc):
+        """
+        Function imports a csv file and updates the object with locations
+        """
+        pass
+
+    def is_valid(self):
+        """
+        Function returns bool if all of the required file locations are added.
+        """
+        pass
