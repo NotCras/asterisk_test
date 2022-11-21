cb23aeeb064dd6f385cc2946c04cee8b6f078c01
John <morrowjo@oregonstate.edu>
2022-11-21 02:56:04 -0800

    updates to process

--- asterisk_test/2_data_processing.py
+++ asterisk_test/2_data_processing.py
@@ -2,6 +2,7 @@ import pdb
 from pathlib import Path
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 from file_manager import AstDirectory
 from ast_hand_translation import AstHandTranslation
@@ -22,20 +23,23 @@ new_ast_files.debug_figs = data_directory / "results" / "debug_plots"
 resources_home = Path(__file__).parent.parent.absolute()
 new_ast_files.resources = resources_home.parent / "resources"
 
-rotation_type = "x"
-for hand in ["2v2", "2v3", "3v3", "p2vp2", "2v1", "p1vp1"]:
-    print(hand)
-    hand_data = AstHandTranslation(new_ast_files, hand_name=hand, rotation=rotation_type)
-    hand_data.load_trials()
-    print(list(hand_data.data.keys()))
-    hand_data.filter_data()
+#rotation_type = "x"
+for rotation_type in ["x", "p15", "m15"]:
+    for hand in ["2v2", "2v3", "3v3", "p2vp2", "2v1", "p1vp1"]:
+        print(hand)
+        hand_data = AstHandTranslation(new_ast_files, hand_name=hand, rotation=rotation_type)
+        hand_data.load_trials()
+        print(list(hand_data.data.keys()))
+        hand_data.filter_data()
 
-    trials = hand_data.data
-    aplt.plot_asterisk(new_ast_files, dict_of_trials=trials, hand_name=hand, show_plot=False, save_plot=True)
+        trials = hand_data.data
+        aplt.plot_asterisk(new_ast_files, dict_of_trials=trials, hand_name=hand, show_plot=False, save_plot=True)
 
-    hand_data.calc_averages(save_debug_plot=True)
-    hand_data.plot_avg_asterisk(show_plot=False, save_plot=True)
-    hand_data.save_all_data()
-    print("======")
-    print(" ")
+        hand_data.calc_averages(save_debug_plot=True)
+        hand_data.plot_avg_asterisk(show_plot=False, save_plot=True)
+        hand_data.save_all_data()
+        print("======")
+        print(" ")
+
+        plt.close("all")
 
